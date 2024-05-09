d573284a57aacd8f2edd43e9c05edfebc074fbd6
Elanur Dingin <73476047+elanurdngn@users.noreply.github.com>
2024-05-09 15:07:13 +0300

    Add files via upload

---
+++ interfacesDemo/src/OutsourceWorker.java
@@ -0,0 +1,11 @@
+public class OutsourceWorker implements IWorkable,IPayable{
+    @Override
+    public void pay() {
+
+    }
+
+    @Override
+    public void work() {
+
+    }
+}
---
+++ interfacesDemo/src/Worker.java
@@ -0,0 +1,16 @@
+public class Worker implements IPayable,IEatable, IWorkable {
+    @Override
+    public void eat() {
+
+    }
+
+    @Override
+    public void pay() {
+
+    }
+
+    @Override
+    public void work() {
+
+    }
+}
